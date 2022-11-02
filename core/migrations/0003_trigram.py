052d2a0efb5ea46ea18cf9fe6b810b038970fb04
Matthew Pugh <pughmds@cyram.org>
2022-11-02 22:47:23 +0000

    Updated actions to work with postgres settings

--- core/migrations/0003_trigram.py
+++ core/migrations/0003_trigram.py
@@ -1,6 +1,7 @@
 from django.contrib.postgres.operations import TrigramExtension
 from django.db import migrations
 
+
 class Migration(migrations.Migration):
     dependencies = [
         ("core", "0002_person_address_person_cms_id_person_date_of_birth_and_more"),
