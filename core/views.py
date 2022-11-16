be2ae657d03b6c2c02d6901395166f822f5fc027
Matthew Pugh <pughmds@cyram.org>
2022-11-16 14:55:20 +0000

    Working person service involvement dropdown

--- core/views.py
+++ core/views.py
@@ -1,11 +1,13 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.postgres.search import TrigramSimilarity
 from django.db.models import Q
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseBadRequest, HttpResponseRedirect, JsonResponse
 from django.shortcuts import redirect, render
+from django.template.loader import render_to_string
 from django.urls import reverse
 
 from .forms import CmsIdForm, NameSearchForm
+from .helpers.shared import find_service_involvement_count
 from .models import Person, ServiceSummary
 
 
@@ -107,9 +109,82 @@ def person(request, person_id):
     p = Person.objects.get(id=person_id)
     relation_count = len(p.relationships.all()) + len(p.reverse_relationships.all())
     s = ServiceSummary.objects.all()
+    for service in s:
+        service.records = find_service_involvement_count(service, p)
 
     return render(
         request,
         "person.html",
         {"person": p, "relation_count": relation_count, "service_list": s},
     )
+
+
+@login_required()
+def get_service_records(request, person_id, service_id):
+    is_ajax = request.headers.get("X-Requested-With") == "XMLHttpRequest"
+    if is_ajax:
+        if request.method == "GET":
+            service_record = ServiceSummary.objects.get(id=service_id)
+            person = Person.objects.filter(id=person_id).first()
+
+            records = [
+                service_record.service_adult_social_care_records.filter(
+                    person=person
+                ).values(
+                    "local_authority_organisation",
+                    "contact",
+                    "service_involvement",
+                    "start_date_of_last_involvement",
+                    "date_of_most_recent_interaction",
+                    "date_current_as_of",
+                    "coverage_start_date",
+                    "coverage_end_date",
+                    "coverage_geographic_area",
+                    "other",
+                ),
+                service_record.service_school_records.filter(person=person).values(
+                    "school_name",
+                    "contact_number",
+                    "admission_type",
+                    "service_involvement",
+                    "start_date_of_last_involvement",
+                    "date_of_most_recent_interaction",
+                    "date_current_as_of",
+                    "coverage_start_date",
+                    "coverage_end_date",
+                    "coverage_geographic_area",
+                    "other",
+                ),
+                service_record.service_housing_records.filter(person=person).values(
+                    "housing_association",
+                    "contact",
+                    "tenancy_start",
+                    "antisocial_behaviour",
+                    "rent_arrears",
+                    "notice_seeking_possession",
+                    "eviction",
+                    "service_involvement",
+                    "start_date_of_last_involvement",
+                    "date_of_most_recent_interaction",
+                    "date_current_as_of",
+                    "coverage_start_date",
+                    "coverage_end_date",
+                    "coverage_geographic_area",
+                    "other",
+                ),
+                service_record.service_police_records.filter(person=person).values(
+                    "police_area", "contact", "other"
+                ),
+            ]
+
+            html = render_to_string(
+                "person_service_records.html",
+                {"service": service_record, "records": records},
+            )
+            html = html.replace("\n", "").replace('"', "")
+            return JsonResponse(html, safe=False)
+        else:
+            return JsonResponse({"status": "Invalid request"}, status=400)
+
+    else:
+        return HttpResponseBadRequest("Invalid request")
