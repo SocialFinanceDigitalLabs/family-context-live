146a99f00515cf785270e619546adcf93fa381e8
Tambe Tabitha <tambe.tabitha@socialfinance.org.uk>
2023-10-17 16:55:41 +0100

    enabling multi-file processing

--- core/management/commands/data_intake.py
+++ core/management/commands/data_intake.py
@@ -8,17 +8,17 @@ class Command(BaseCommand):
     help = "populates the database with data from a csv file"
 
     def add_arguments(self, parser: CommandParser) -> None:
-        parser.add_argument("file", type=str)
+        parser.add_argument("files", nargs="+", type=str)
 
-    def handle(self, *args, **kwargs):
-        data = pd.read_excel(kwargs["file"])
-        filename = kwargs["file"].split("/")[-1].split(".")[0]
+    def update_db(self, file) -> None:
+        data = pd.read_csv(file)
+        filename = file.split("\\")[-1].split(".")[0]
 
         # standardise id column names
         cols_as_expected = {
-            "Last name": "last_name",
-            "First name": "first_name",
-            "Date of birth": "date_of_birth",
+            "Last Name": "last_name",
+            "First Name": "first_name",
+            "Date of Birth": "date_of_birth",
             "Address": "address",
             "Age": "age",
             "NHS No": "nhs_number",
@@ -38,6 +38,10 @@ class Command(BaseCommand):
             orient="records"
         )
 
+        datasource = DataSource.objects.create(
+            name=filename,
+            last_update=timezone.now(),
+        )
         for ind, row in data.iterrows():
             person = Person.objects.create(
                 last_name=row["last_name"],
@@ -47,12 +51,21 @@ class Command(BaseCommand):
                 age=row["age"],
                 nhs_number=row["nhs_number"],
             )
-            datasource = DataSource.objects.create(
-                name=filename,
-                last_update=timezone.now(),
-            )
             record = Record.objects.create(
                 person_id=person,
                 datasource_id=datasource,
                 record=json_records[ind],
             )
+
+    def handle(self, *args, **kwargs):
+        Record.objects.all().delete()
+        DataSource.objects.all().delete()
+        Person.objects.all().delete()
+
+        for file in kwargs["files"]:
+            self.update_db(file)
+        
+        self.stdout.write(f"Records: {len(Record.objects.all())}")
+        self.stdout.write(f"Persons: {len(Person.objects.all())}")
+        self.stdout.write(str(DataSource.objects.all()))
+        self.stdout.write(self.style.SUCCESS("Database updated successfully"))
