9a8b20e598875aa73bc9190ea3c5822c63167d6c
Tambe Tabitha <tambe.tabitha@socialfinance.org.uk>
2023-10-23 14:06:27 +0100

    localhost as default origin

--- family_context/settings.py
+++ family_context/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = config(
 )
 
 CSRF_TRUSTED_ORIGINS = config(
-    "TRUSTED_ORIGINS", cast=lambda v: [s.strip() for s in v.split(",")], default=""
+    "TRUSTED_ORIGINS", cast=lambda v: [s.strip() for s in v.split(",")], default="https://127.0.0.1"
 )
 
 
