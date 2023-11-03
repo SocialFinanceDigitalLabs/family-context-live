726a9c5c3073bf50fe6e0a4058ec2556a49798b6
Matthew Pugh <pughmds@cyram.org>
2023-11-03 10:12:55 +0000

    Fix/database switching (#56)
    * Improved Database switching and the DJ_DATABASE_URL config setting logic

--- family_context/settings.py
+++ family_context/settings.py
@@ -148,23 +148,23 @@ WEBPACK_LOADER = {
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 MAX_CONN_AGE = 600
+DATABASE_URL = config("DATABASE_URL", default=False)
 
-DATABASES = {
-    "default": dj_database_url.config(
-        default=config("DATABASE_URL", f"sqlite:///{BASE_DIR}/db.sqlite3"),
-        conn_max_age=MAX_CONN_AGE,
-        ssl_require=False,
-    ),
-}
-
-database_url = config("DATABASE_URL", default=False)
-MAX_CONN_AGE = 600
-
-if database_url:
-    # Configure Django for DATABASE_URL environment variable.
-    DATABASES["default"] = dj_database_url.config(
-        conn_max_age=MAX_CONN_AGE, ssl_require=True
-    )
+if DATABASE_URL:
+    DATABASES = {
+        "default": dj_database_url.config(
+            default=DATABASE_URL,
+            conn_max_age=MAX_CONN_AGE,
+        ),
+    }
+else:
+    DATABASES = {
+        "default": dj_database_url.config(
+            default="sqlite://{BASE_DIR}/db.sqlite3",
+            conn_max_age=MAX_CONN_AGE,
+            ssl_require=False,
+        ),
+    }
 
 
 # Password validation
