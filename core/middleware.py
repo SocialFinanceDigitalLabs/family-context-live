9d9c15daa3237c79e18a63213b73458f487d285f
Tambe Tabitha <tambe.tabitha@socialfinance.org.uk>
2023-11-07 16:46:38 +0000

    store logs in log file

--- core/middleware.py
+++ core/middleware.py
@@ -1,13 +1,15 @@
-from .models import PageViewLog
+import logging
+
+logging.basicConfig(filename="user_activity.log", encoding="utf-8", level=logging.DEBUG, format="%(asctime)s [%(levelname)s] - %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
+logging.info("Started logging")
 
 class PageViewLoggerMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        PageViewLog.objects.create(
-            user = request.user,
-            url = request.path
-        )
+        user = request.user if request.user.is_authenticated else "AnonymousUser"
+        url = request.path
+        logging.info(f"User: {user} - URL: {url}")
         response = self.get_response(request)
         return response
--- core/models.py
+++ core/models.py
@@ -31,10 +31,3 @@ class Record(models.Model):
     def __str__(self):
         return self.record
     
-class PageViewLog(models.Model):
-    user = models.CharField(max_length=70, blank=True, null=True)
-    url = models.CharField(max_length=70, blank=True, null=True)
-    timestamp = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.url
