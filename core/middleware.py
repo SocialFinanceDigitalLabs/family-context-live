42bf1066b0042f907da57ee02f47135b790734c7
Tambe Tabitha <tambe.tabitha@socialfinance.org.uk>
2023-11-16 13:47:57 +0000

    user id not name

--- core/middleware.py
+++ core/middleware.py
@@ -5,7 +5,7 @@ class PageViewLoggerMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
-        user = request.user if request.user.is_authenticated else "AnonymousUser"
+        user = request.user.id if request.user.is_authenticated else "AnonymousUser"
         url = request.path
 
         logger =  logging.getLogger(__name__)
