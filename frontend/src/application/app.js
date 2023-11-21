06a2c0ab6ed89873e68b1f10903dc55dba2dac1e
Tambe Tabitha <tambe.tabitha@socialfinance.org.uk>
2023-11-21 12:07:54 +0000

    click or timeout works.

--- frontend/src/application/app.js
+++ frontend/src/application/app.js
@@ -3,4 +3,30 @@ import M from "materialize-css/dist/js/materialize.min";
 
 window.document.addEventListener("DOMContentLoaded", function () {
 M.AutoInit();
+AlertInit();
 });
+
+const AlertInit = () => {
+    const removeAlert = (alertref) => {
+        alertref.remove();
+    }
+
+
+    const alerts = document.querySelectorAll(".alerts");
+    alerts.forEach((alert) => {
+        let timeout = alert.getAttribute("data-timeout"); 
+        timeout = timeout ? Number(timeout) : null;
+        
+        if (timeout) {
+            setTimeout(() => {
+                removeAlert(alert);
+            }, timeout);
+        }
+        
+        alert.addEventListener("click", (evt) => {
+            evt.preventDefault();
+            removeAlert(alert);
+        })
+    })
+}
+ 
