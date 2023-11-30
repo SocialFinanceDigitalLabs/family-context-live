328f38e1e12c521f62c85de7327a637a08e13ea8
Tambe Tabitha <tambe.tabitha@socialfinance.org.uk>
2023-11-30 11:22:05 +0000

    setInterval works

--- frontend/src/application/app.js
+++ frontend/src/application/app.js
@@ -11,22 +11,31 @@ const AlertInit = () => {
         alertref.remove();
     }
 
-
+    let currentTime = 0
     const alerts = document.querySelectorAll(".alerts");
-    alerts.forEach((alert) => {
-        let timeout = alert.getAttribute("data-timeout"); 
-        timeout = timeout ? Number(timeout) : null;
-        
-        if (timeout) {
-            setTimeout(() => {
-                removeAlert(alert);
-            }, timeout);
-        }
-        
-        alert.addEventListener("click", (evt) => {
-            evt.preventDefault();
-            removeAlert(alert);
+    const timeoutRemoveAlert= (alerts)=>{
+        alerts.forEach((alert) => {
+            let timeout = alert.getAttribute("data-timeout"); 
+            timeout = timeout ? Number(timeout) : null;
+            
+            if (timeout && timeout>= currentTime) {
+                    removeAlert(alert);
+            }
         })
+    }
+
+    if (alerts.length > 0){
+        setInterval(()=>{
+            console.log("interval running");
+            currentTime += 1000;
+            timeoutRemoveAlert(alerts);
+        }, 1000)
+
+    }
+
+    alert.forEach.addEventListener("click", (evt) => {
+        evt.preventDefault();
+        removeAlert(alert);
     })
+    
 }
- 
